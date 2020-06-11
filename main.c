097b5f9b96da1dc1ee5792819897d379b057c4ef
RicGrimaldo <rgrimaldo2307@gmail.com>
2020-06-11 16:21:13 -0500

    En algunos for se agregó que terminen cuando esté vacío como modo de precaución

--- main.c
+++ main.c
@@ -176,7 +176,7 @@ int Encontrar_cadena (char cad1[],char cad2[]){
 int parentesis_paridad(char a[],HWND hwnd){
     int b=0,c=0,error=0,m; /*Contadores para contar cantidad de paréntesis en la cadena*/
     m=strlen(a);
-    for(int i=0;i<m;i++){
+    for(int i=0;i<m,a[i+1]!='\0';i++){
         switch(a[i]){
         case '(': b++;
         break;
@@ -199,7 +199,7 @@ int parentesis_paridad(char a[],HWND hwnd){
 int parentesis_vacio(char a[], HWND hwnd){
     int error=0;
     for(int i=0;i<strlen(a);i++){
-        if(a[i]=='('&& a[i+1]==')'){/*Al encontrar un par de paréntesis vacíos, se sale del bucle*/
+        if(a[i]=='('&& a[i+1]==')' && a[i+1]!='\0'){/*Al encontrar un par de paréntesis vacíos, se sale del bucle*/
                 MessageBox(hwnd,"Error al haber dejado un par de paréntesis vacíos","Error sintáctico",MB_ICONWARNING | MB_OK);
                 return error=1;
         }
@@ -229,7 +229,7 @@ int validacion_caracter(char a[],HWND hwnd){
 	}
 
 	if(error==0){
-		for(int i=1;i<strlen(a);i++){
+		for(int i=1;i<strlen(a),a[i+1]!='\0';i++){
 			if(a[i-1]=='(' && es_operador(a[i])==1 && a[i]!='(' && a[i]!='-' && esDigito(a[i])==0){
 				error=1; /*Validará que el primer carácter no sea ningún operador a excepción del '(', y cualquier primer letra de las funciones*/
                 MessageBox(hwnd,"Error con el primer carácter después del paréntesis","Error sintáctico",MB_ICONWARNING | MB_OK);
